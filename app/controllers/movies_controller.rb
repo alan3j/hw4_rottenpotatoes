f0b49a753f8cfb229b132d3fd5d02e671a8fc073
Craig J <alan.jjj@gmail.com>
2013-02-24 22:12:28 -0600

    Validation updates to Controller and View

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -37,13 +37,19 @@ class MoviesController < ApplicationController
   end
 
   def new
-    # default: render 'new' template
+    @movie = Movie.new
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+
+    @movie = Movie.new(params[:movie])
+    if @movie.save
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movies_path
+    else
+      render :new # Note new template can access @movie's field values!
+    end
+
   end
 
   def edit
@@ -51,10 +57,15 @@ class MoviesController < ApplicationController
   end
 
   def update
+
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
+    if @movie.update_attributes(params[:movie])
+      flash[:notice] = "#{@movie.title} was successfully updated."
+      redirect_to movies_path
+    else
+      render :edit
+    end
+
   end
 
   def destroy
