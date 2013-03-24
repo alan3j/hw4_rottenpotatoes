0166fc6436af926df33d4748841722140d8d15f7
Craig J <alan.jjj@gmail.com>
2013-03-24 13:08:14 -0500

    Add JScript AJAX from chapter 11.6

--- app/assets/javascripts/rotten_potatoes.js
+++ app/assets/javascripts/rotten_potatoes.js
@@ -27,3 +27,37 @@ RP = {
 }
 $(RP.setup);       // when document ready, run setup code
 
+RP2 = {
+    setup: function() {
+        // add invisible 'div' to end of page:
+        $('<div id="movieInfo"></div>').
+            hide().
+            appendTo($('body'));
+        $('#movies a').click(RP2.getMovieInfo);
+    },
+    getMovieInfo: function() {
+        $.ajax({type: 'GET',
+                url: $(this).attr('href'),
+                timeout: 5000,
+                success: RP2.showMovieInfo,
+                error: function() { alert('Error!'); }
+               });
+        return(false);
+    },
+    showMovieInfo: function(data) {
+        // center a floater 1/2 as wide and 1/4 as tall as screen
+        var oneFourth = Math.ceil($(window).width() / 4);
+        $('#movieInfo').
+            html(data).
+            css({'left': oneFourth,  'width': 2*oneFourth, 'top': 250}).
+            show();
+        // make the Close link in the hidden element work
+        $('#closeLink').click(RP2.hideMovieInfo);
+        return(false);  // prevent default link action
+    },
+    hideMovieInfo: function() {
+        $('#movieInfo').hide(); 
+        return(false);
+    },
+}
+$(RP2.setup);
